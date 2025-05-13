da33c5af7c646834bfdd7b337c1747ae859b642b
Van-Excel <vanexcel.investments@gmail.com>
2025-05-13 13:28:35 +0000

    musings-binary and hex

--- musing.py
+++ musing.py
@@ -1,2 +1,29 @@
 
 print('"{0}" is the "{0}" of that "{1}"'.format('Vanexcel','firstname', 'ghanaian'))
+
+
+import struct
+
+
+payload = 'Vanexcel'
+payload_byte = payload.encode("utf-8")
+print(f'payload: {payload}')
+print(f"payload_bytes: {payload_byte}")
+
+
+payload2 = "Vanexcel"
+
+print("Number |String | ASCII  |  UTF-8  |  BYTES")
+
+for index, char in enumerate(payload2, start=1):
+    string = char
+    ascii = ord(char)
+    utf8 = ord(char)
+    byteHex = hex(ord(char))
+    byteBinary = bin(ord(char))
+
+    print(f"{index}  |{string} |  {ascii} |  {utf8} |  {byteHex} |  {byteBinary}")
+
+# learning alot.
+# applying my new knowledge of bytes and memory
+
